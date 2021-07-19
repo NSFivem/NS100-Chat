b1249dcb1d2c289c5d9f3d9ab7b3f33aa41c7f1e
NS100 <86939732+NSFivem@users.noreply.github.com>
2021-07-18 21:31:55 -0400

    Update __resource.lua

--- esx_rpchat/__resource.lua
+++ esx_rpchat/__resource.lua
@@ -1,12 +1,7 @@
---[[
-
-  ESX RP Chat
-
---]]
-
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
-description 'ESX RP Chat'
+description 'Chat addon made by NS100'
+author 'NS100#0001'
 
 version '1.0.0'
 
@@ -18,3 +13,5 @@ server_scripts {
   'server/main.lua'
 
 }
+
+--any errors occur contact me via discord!
